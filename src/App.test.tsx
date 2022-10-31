d046ab905c0adc29b40a247bd77e520dc8671062
Casira Copes <cjcopes@gmail.com>
2022-10-31 15:08:45 -0400

    first edits

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-test("renders the course name somewhere", () => {
+test("renders my name somewhere", () => {
     render(<App />);
-    const linkElement = screen.getByText(/CISC275/i);
+    const linkElement = screen.getByText(/Casira/i);
     expect(linkElement).toBeInTheDocument();
 });
